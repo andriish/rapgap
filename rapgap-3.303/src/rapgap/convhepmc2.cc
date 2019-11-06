d65dc2938cae94db3d7452ebc61777e70b36e1a5
andriish <andriish@users.noreply.github.com>
2019-11-06 10:22:47 +0100

    OK

--- rapgap-3.303/src/rapgap/convhepmc2.cc
+++ rapgap-3.303/src/rapgap/convhepmc2.cc
@@ -1,3 +1,5 @@
+#ifndef Pythia6_Pythia6ToHepMC2_H
+#define Pythia6_Pythia6ToHepMC2_H
 #include <iostream>
 #include "HepMC/PythiaWrapper.h"
 #include "HepMC/IO_HEPEVT.h"
@@ -7,55 +9,36 @@
 #include "HepMC/HEPEVT_Wrapper.h"
 #include "PythiaHelper.h"
 
-using namespace std;
-
-extern "C" {
-    HepMC::GenEvent* event_hepmc2=NULL;
-    HepMC::IO_HEPEVT hepevent_hepmc2io;
-    HepMC::IO_GenEvent* ascii_io=NULL;
-    int  inithepmc_(char * outfile )
+std::map<int,std::pair<HepMC::IO_GenEvent*,HepMC::GenEvent*> > hepmc2_gWriters;
+HepMC::IO_HEPEVT  hepmc2_gHEPEVT;
+HepMC::GenEvent* hepmc2_gWriters_get_event(const int & position)
     {
-        if (strlen(outfile)>0)
-            ascii_io=new HepMC::IO_GenEvent(outfile ,std::ios::out);
-        else
-            ascii_io=new HepMC::IO_GenEvent("rapgap.hepmc" ,std::ios::out);
+     return    hepmc2_gWriters[position].second;
+     }
+using namespace HepMC;
+extern "C" {
+    int getorig_(int &a);
+    int hepmc2_delete_writer_(const int & position)
+    {
         return 0;
     }
-    int getorig_(int &a);
-
-
-    int convhepmc_(int & ievent, int & iproc, double & xsec, double & xsece,
-                    int& flav1, int& flav2,double &  x1,double &  x2,double &  q2pdfeval,
-                    double &  xf1mom, double & xf2mom ,int&  pdf1,int& pdf2
-                   ) {
-
-
-        hepevent_hepmc2io.set_trust_mothers_before_daughters( true );
+    int hepmc2_convert_event_(const int & position)
+    {
+        hepmc2_gHEPEVT.set_trust_mothers_before_daughters( true );
 
         // pythia pyhepc routine convert common PYJETS in common HEPEVT
         call_pyhepc( 1 );
+        GenEvent* event_hepmc2=hepmc2_gWriters[position].second;
         if (event_hepmc2) delete event_hepmc2;
-        event_hepmc2 = hepevent_hepmc2io.read_next_event();
+        
+        event_hepmc2= hepmc2_gHEPEVT.read_next_event();
+        hepmc2_gWriters[position].second  = event_hepmc2;
         event_hepmc2->use_units(HepMC::Units::GEV, HepMC::Units::MM);
-        event_hepmc2->set_event_number(ievent);
-        event_hepmc2->set_signal_process_id(iproc);
-        event_hepmc2->set_mpi(-1);
-        event_hepmc2->set_event_scale(q2pdfeval);
-        event_hepmc2->set_alphaQED(-1);
-        event_hepmc2->set_alphaQCD(-1);
-        std::vector<long> rs;
-        rs.push_back(1);
-        event_hepmc2->set_random_states(rs);
+
 
         //Set beams
         event_hepmc2->barcode_to_particle(1)->set_status(4);
         event_hepmc2->barcode_to_particle(2)->set_status(4);
-//        for (HepMC::GenEvent::particle_iterator  p=event_hepmc2->particles_begin(); p!=event_hepmc2->particles_end(); p++)
-//        (*p)->set_barcode_(10000+(*p)->barcode());
-        //Set PDF info
-        HepMC::PdfInfo pdf( flav1, flav2, x1, x2, q2pdfeval, xf1mom, xf2mom , pdf1,pdf2);
-        event_hepmc2->set_pdf_info(pdf);
-
 
         //Fix problems with broken record
         //1) Detached FSR photons and stable electrons with end vertex
@@ -88,7 +71,6 @@ extern "C" {
                 (*p)->end_vertex()->add_particle_out(P);
                 created_proper_final_electron=true;// Doing that just once. Not sure if there are cases where nore than one is needed.
             }
-        
         }
 
         //2) ISR photons in interaction vertex
@@ -129,18 +111,91 @@ extern "C" {
             VI->add_particle_in(ER);
         }
 
-        event_hepmc2->weights().push_back(1.0);
-
-        //      std::cout << " ievent " << ievent << " iproc " << iproc << " xsec " <<xsec<< std::endl;
-        // set cross section information set_cross_sectio( xsec, xsec_err)
-        const double xsecval = xsec;
-        const double xsecerr = xsece ;
+        return 0;
+    }
+    int hepmc2_write_event_(const int & position)
+    {
+    (*hepmc2_gWriters[position].first) << hepmc2_gWriters[position].second;
+        return 0;
+    }
+    int hepmc2_clear_event_(const int & position)
+    {
+        hepmc2_gWriters[position].second->clear();
+        return 0;
+    }
+    int hepmc2_set_cross_section_(const int & position, const double& x,const double& xe, const int& n1,const int& n2)
+    {
+        const double xsecval = x;
+        const double xsecerr = xe ;
         HepMC::GenCrossSection cross;
         cross.set_cross_section( xsecval, xsecerr );
-        event_hepmc2->set_cross_section( cross );
-        // write the event out to the ascii file
-        if (ascii_io)
-        (*ascii_io) << event_hepmc2;
-    return 0;
+        hepmc2_gWriters[position].second->set_cross_section( cross );
+        return 0;
     }
+
+    int hepmc2_set_pdf_info_(const int & position,const int& parton_id1, const int& parton_id2, const double& x1, const double& x2,
+                      const double& scale_in, const double& xf1,const double& xf2,
+                      const int& pdf_id1, const int& pdf_id2)
+    {
+        HepMC::PdfInfo pdf( parton_id1, parton_id2,x1,x2,scale_in,xf1,xf2,pdf_id1, pdf_id2);
+        hepmc2_gWriters[position].second->set_pdf_info(pdf);
+        return 0;
+    }
+
+    int hepmc2_set_hepevt_address_(int* a)
+    {
+            return 0;
+    }
+    int hepmc2_set_attribute_int_(const int & position,const int & attval,const char* attname)
+    {
+		std::string sta(attname);
+		GenEvent* event_hepmc2=hepmc2_gWriters[position].second;
+		if (sta==std::string("mpi")) { event_hepmc2->set_mpi(attval); return 0;}
+		if (sta==std::string("signal_process_id")) { event_hepmc2->set_signal_process_id(attval); return 0;}
+		//        std::vector<long> rs;
+//        rs.push_back(1);
+//        event_hepmc2->set_random_states(rs);
+        return 0;
+    }
+    int hepmc2_set_attribute_double_(const int & position,const double & attval,const char* attname)
+    {
+		std::string sta(attname);
+		GenEvent* event_hepmc2=hepmc2_gWriters[position].second;
+		if (sta==std::string("alphaQED")) { event_hepmc2->set_alphaQED(attval); return 0;}
+		if (sta==std::string("alphaQCD")) { event_hepmc2->set_alphaQCD(attval); return 0;}
+		if (sta==std::string("event_scale")) { event_hepmc2->set_event_scale(attval); return 0;}
+        return 0;
+    }
+
+    int hepmc2_new_writer_(const int & position,const int & mode,const char* ffilename)
+    {
+		
+		        int r_position=position;
+        if (r_position==0)
+        {
+            if (hepmc2_gWriters.size()==0) r_position=1;
+            if (hepmc2_gWriters.size()!=0) r_position=hepmc2_gWriters.rend()->first+1;
+        }
+        if (hepmc2_gWriters.find(r_position)!=hepmc2_gWriters.end()) {
+            printf("Error in %s: Writer at position %i already exists\n",__FUNCTION__,r_position);
+            exit(1);
+        }
+		
+		hepmc2_gWriters[r_position]=std::pair<IO_GenEvent*,GenEvent*>( new HepMC::IO_GenEvent(ffilename ,std::ios::out),new GenEvent(Units::GEV,Units::MM));
+        return  r_position;
+    }
+    int hepmc2_new_weight_(const int & position, const char* name)
+    {
+        return 0;
+    }
+    int hepmc2_set_weight_by_index_(const int & position,const double& val, const int & pos)
+    {
+        hepmc2_gWriters[position].second->weights()[pos]=val;
+        return 0;
+    }
+    int hepmc2_set_weight_by_name_(const int & position,const double& val, const char* name)
+    {
+        return 0;
+    }    
 }
+#endif
---
+++ rapgap-3.303/src/rapgap/convrivet2-dummy.cc
@@ -0,0 +1,26 @@
+extern "C" {
+    int rivet_interface_version_()
+    {
+        return 0;
+    }    
+    int rivet_init_(char* rname1) 
+    {
+        return 0;
+    }
+    int rivet_initfirst_event_()
+    {
+        return 0;
+    }
+    int rivet_run_() 
+    {
+        return 0;
+    }
+    int rivet_add_analysis_(char* ana) 
+    {
+        return   0;
+    }
+    int rivet_done_(char* filename1) 
+    {
+        return 0;
+    }
+}
